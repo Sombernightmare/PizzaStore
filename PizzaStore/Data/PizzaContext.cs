5a9f1e393b8c082feeeaab3a4236981c36bb7bfb
sombernightmare <timothytallen@gmail.com>
2019-07-15 23:09:04 -0500

    Started working on the Employee controller and Employee class. Connected to localhost database.

---
+++ PizzaStore/Data/PizzaContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaStore.Data
+{
+    public class PizzaContext: DbContext
+    {
+        public PizzaContext(DbContextOptions<PizzaContext> options): base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PizzaStore.Data;
 
 namespace PizzaStore
 {
@@ -14,7 +16,24 @@ namespace PizzaStore
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+          var host = CreateWebHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<PizzaContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Error occured while seeding DB");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
